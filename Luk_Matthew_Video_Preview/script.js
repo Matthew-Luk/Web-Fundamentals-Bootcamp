6b8e562923135bb68c1154a7b374d93e30629261
Matthew Luk <Matthew.Luk@yahoo.com>
2022-05-17 16:07:00 -0700

    Add files via upload
    CSS and JS for the project.

---
+++ Luk_Matthew_Video_Preview/script.js
@@ -0,0 +1,12 @@
+// I did not write or claim to have written the HTML or CSS for this project, it was provided to me by my coding bootcamp instructor. I only wrote part of the javascript.
+console.log("page loaded...");
+
+function preview(element) {
+    console.log(element);
+    element.play();
+}
+
+function pause(element) {
+    console.log(element);
+    element.pause();
+}
