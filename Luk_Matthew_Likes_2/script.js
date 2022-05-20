d8620b3da2c253f79da907c7f2801324cbd3549d
Matthew Luk <Matthew.Luk@yahoo.com>
2022-05-19 19:10:28 -0700

    Add files via upload
    JS for the assignment.

---
+++ Luk_Matthew_Likes_2/script.js
@@ -0,0 +1,23 @@
+var count1 = 0
+var count2 = 0
+var count3 = 0
+var countElement = document.querySelector("#count")
+var countElement2 = document.querySelector("#count2")
+var countElement3 = document.querySelector("#count3")
+
+console.log(countElement)
+
+function like1() {
+    count1 += 1;
+    countElement.innerText = count1 + " like(s)";
+}
+
+function like2() {
+    count2 += 1;
+    countElement2.innerText = count2 + " like(s)";
+}
+
+function like3() {
+    count3 += 1;
+    countElement3.innerText = count3 + " like(s)";
+}
