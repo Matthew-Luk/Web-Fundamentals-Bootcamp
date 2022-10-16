a32a00dd74628b0a66f0848485c6e3246fdfae35
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-15 23:12:12 -0700

    Update script.js
    updated JS to reflect different colors

--- Luk_Matthew_Power_Up_4/script.js
+++ Luk_Matthew_Power_Up_4/script.js
@@ -1,13 +1,12 @@
-//I did not write or claim to have written the HTML or CSS for this project, it was provided to me by my coding bootcamp instructor. I only wrote part of the javascript. 
-
 var whatIWantToChange = document.getElementById("whatIWantToChange");
 
 function myFunction(element){
-    element.style.color = "black";
+    element.style.color = "aqua";
+    element.style.backgroundColor = "orange";
 }
 
 function myOtherFunction(myString){
     var element = document.getElementById(myString);
-    element.style.color = "white"
-    element.style.fontSize = "xx-large"
+    element.style.color = "red"
+    element.style.fontSize = "1000%"
 }
