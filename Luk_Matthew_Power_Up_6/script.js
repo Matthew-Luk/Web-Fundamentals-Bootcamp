6700455790da08baec97fccd146cdc722c4cdfa8
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-20 20:47:54 -0700

    Update script.js
    fixed and tested functions

--- Luk_Matthew_Power_Up_6/script.js
+++ Luk_Matthew_Power_Up_6/script.js
@@ -1,30 +1,34 @@
-var array = ["matthew", "abby", "josh", "jose"]
-
 // Function that Logs out even numbers to 50
 function evento50() {
     for(i = 0; i < 51; i+=2) {
         console.log(i)
     }
 }
+evento50()
 
 // Function that logs off numbers to 13
 function oddto13 () {
-    for(i = 0; i < 14; i += 2) {
+    for(i = 1; i < 14; i += 2) {
         console.log(i)
     }
 }
+oddto13()
 
 // Function that logs out all names from an array
 function nameArray() {
+    var array = ["matthew", "abby", "josh", "jose"]
     for(i = 0; i < array.length; i++) {
         console.log(array[i])
     }
 }
+nameArray()
 
 // Function that adds numbers to an array
 function addArray() {
+    var arr = []
     for(i = 0; i <= 2; i++) {
-        array.push(i)
+        arr.push(i)
     }
-    console.log(array)
+    console.log(arr)
 }
+addArray()
