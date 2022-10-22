baed4475ff6c6e036d7a7572d532c73c043ee699
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-21 22:05:47 -0700

    Update script.js

--- Luk_Matthew_Video_Preview/script.js
+++ Luk_Matthew_Video_Preview/script.js
@@ -1,4 +1,3 @@
-// I did not write or claim to have written the HTML or CSS for this project, it was provided to me by my coding bootcamp instructor. I only wrote part of the javascript.
 console.log("page loaded...");
 
 function preview(element) {
