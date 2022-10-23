a47151bb656e775501dc590a937db18ee072e8a2
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-22 22:24:32 -0700

    Update script.js
    added functionality for dislike button

--- Luk_Matthew_Likes/script.js
+++ Luk_Matthew_Likes/script.js
@@ -5,5 +5,10 @@ console.log(countElement)
 
 function like() {
     count += 1;
+    countElement.innerHTML = count + " like(s)";
+}
+
+function dislike() {
+    count -= 1;
     countElement.innerText = count + " like(s)";
 }
