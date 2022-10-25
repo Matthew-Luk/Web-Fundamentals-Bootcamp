d9d9742041afe7daf9f30321a4b5965ee05e83f8
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-24 19:15:19 -0700

    Update script.js
    updated to include random pizza generator function

--- Luk_Matthew_Dojo_Pizzeria/script.js
+++ Luk_Matthew_Dojo_Pizzeria/script.js
@@ -7,6 +7,72 @@ function pizzaOven(crustType, sauceType, cheeses, toppings) {
     return pizza;
 }
 
+var crustTypes = [
+    "deep dish",
+    "hand tossed",
+    "thin and crispy",
+    "cauliflower",
+    "gluten free",
+    "hawaiian bread"
+];
+
+var sauceTypes = [
+    "traditional",
+    "marinara",
+    "bbq",
+    "white sauce",
+    "nacho cheese",
+    "tikka masala"
+];
+
+var cheeses = [
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "swiss cheese",
+    "blue cheese",
+    "goat cheese",
+    "provolone",
+    "parmesan",
+    "vegan cheese"
+];
+
+var toppings = [
+    "pepperoni",
+    "sausage",
+    "chicken",
+    "corn",
+    "olives",
+    "bell peppers",
+    "onions",
+    "mushrooms",
+    "anchovies"
+];
+
+function randomRange(max,min){
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPick(arr){
+    var i = Math.floor(arr.length * Math.random())
+    return arr[i]
+}
+
+function randomPizza(){
+    var pizza = {}
+    pizza.crustType = randomPick(crustTypes)
+    pizza.sauceType = randomPick(sauceTypes)
+    pizza.cheeses = []
+    pizza.toppings = []
+    for(var i=0; i<randomRange(9,1); i++){
+        pizza.cheeses.push(randomPick(cheeses))
+    }
+    for(var i=0; i<randomRange(9,0); i++){
+        pizza.toppings.push(randomPick(toppings))
+    }
+    return pizza
+}
+
 var p1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
 console.log(p1)
 
@@ -18,3 +84,6 @@ console.log(p3)
 
 var p4 = pizzaOven("traditional crust", "creamy garlic", ["mozzarella", "parmesan"], ["bacon", "chicken", "green onion", "diced tomatoes"]);
 console.log(p4)
+
+var p5 = randomPizza()
+console.log(p5)
